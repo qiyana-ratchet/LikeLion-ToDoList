f3b52c8d99bf50fc03967c7060c2422072061418
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-04-28 16:09:35 +0900

    Change structure

--- profile.js
+++ profile.js
@@ -1,48 +1,50 @@
+const input = document.querySelector(".app-insert input");
+const toDos = document.querySelector(".app-list");
+
 // Add task
-document.querySelector(".app-insert input").addEventListener(
-  "keyup",
-  function (event) {
-    if (event.keyCode === 13 && this.value !== "") {
-      addTask(this.value);
-      this.value = "";
+input.addEventListener(  // input 요소에 이벤트 리스너 추가
+  "keyup",  // 키보드 입력
+  function (event) {  // 이벤트 리스너
+    if (event.keyCode === 13) {  // 엔터 입력
+      if (input.value !== "") {  // 입력값이 있을 때만
+        addTask(input.value);  // 입력값 추가
+        input.value = "";  // 입력값 초기화
+      }
     }
   },
-  false
+  false  // 이벤트 버블링 방지
 );
 
-document.querySelector(".app-list").addEventListener(
-  "click",
+toDos.addEventListener(  // toDos 요소에 이벤트 리스너 추가
+  "click",  // 마우스 클릭
   function (event) {
-    // Remove task
-    if (event.target.classList.contains("remove-task")) {
-      removeTask(event.target.parentNode);
+    if (event.target.classList.contains("remove-task")) {  // remove-task 클래스를 가지고 있을 때
+      removeTask(event.target.parentNode);  // 부모 노드 삭제
 
       // Complete Task
-    } else if (event.target.classList.contains("task")) {
-      completeTask(event.target);
+    } else if (event.target.classList.contains("task")) {  // task 클래스를 가지고 있을 때
+      completeTask(event.target);  // task 클래스 토글
     }
   },
-  false
+  false  // 이벤트 버블링 방지
 );
 
+function addTask(task) {  // task 추가
+  var newTask = document.createElement("li");  // li 요소 생성
+  newTask.setAttribute("class", "task");  // li 요소에 class 속성 추가
+  newTask.innerHTML = task + '<a href="javascript:;" class="remove-task">remove</a>';  // li 요소에 내용 추가
 
-function addTask(task) {
-  var new_task = document.createElement("li");
-  new_task.setAttribute("class", "task");
-  new_task.innerHTML =
-    task + '<a href="javascript:;" class="remove-task">remove</a>';
-
-  var $list = document.querySelector(".app-list ul");
-  $list.appendChild(new_task);
+  var list = document.querySelector(".app-list ul");  // ul 요소 선택
+  list.appendChild(newTask);  // ul 요소에 li 요소 추가
 }
 
-function removeTask(task) {
-  task.style.opacity = 0;
-  setTimeout(function () {
-    task.remove();
+function removeTask(task) {  // task 삭제
+  task.style.opacity = 0;  // 투명도 0으로 설정
+  setTimeout(function () {  // 0.4초 후에
+    task.remove();  // task 삭제
   }, 400);
 }
 
-function completeTask(task) {
-  task.classList.toggle("task-complete");
+function completeTask(task) {  // task 완료
+  task.classList.toggle("task-complete");  // task-complete 클래스 토글
 }
